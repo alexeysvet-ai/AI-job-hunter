221b135426fcf1573b5d8f7d2dce5116a2b86aa2
alexeysvet-ai <alexeysvet@gmail.com>
2026-03-20 21:14:24 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+TOKEN = "8754421373:AAFsF9rFolRlWXcNvo1Koahal1CaoMhK420"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Привет! Я AI Job Hunter 🤖")
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("Ты написал: " + message.text)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
