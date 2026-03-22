8917f7397a4a321406e9ad532ab41c90ab65d2db
alexeysvet-ai <alexeysvet@gmail.com>
2026-03-22 12:54:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,9 @@
-import os
 import asyncio
-from aiohttp import web
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-@web.middleware
-async def log_middleware(request, handler):
-    print("REQUEST:", request.method, request.path)
-    return await handler(request)
-
 TOKEN = "8754421373:AAEoHSSZ8hOzaZ6gPORKLx4p0K5TyAWMoys"
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = f"https://ai-job-hunter-production-cc90.up.railway.app/webhook"
-
 bot = Bot(token=TOKEN)
-print("WEBHOOK URL:", WEBHOOK_URL)
-
 dp = Dispatcher()
 
 @dp.message(Command("start"))
@@ -26,40 +14,13 @@ async def start(message: types.Message):
 async def echo(message: types.Message):
     await message.answer("Ты написал: " + message.text)
 
-async def on_startup(app):
-    info = await bot.get_webhook_info()
+async def main():
+    # на всякий случай сбрасываем webhook
+    await bot.delete_webhook(drop_pending_updates=True)
 
-    if info.url != WEBHOOK_URL:
-        print("Setting webhook...")
-        await bot.delete_webhook(drop_pending_updates=True)
-        await bot.set_webhook(WEBHOOK_URL)
-    else:
-        print("Webhook already set")
+    print("Bot started...")
 
-from aiohttp import web
-from aiogram.types import Update
-
-async def handle_webhook(request):
-    try:
-        data = await request.json()
-        print("RAW UPDATE:", data)
-
-        update = Update.model_validate(data)
-        await dp.feed_update(bot, update)
-
-        return web.Response(text="OK")
-
-    except Exception as e:
-        print("ERROR:", e)
-        return web.Response(text="ERROR", status=500)
-
-def main():
-    app = web.Application(middlewares=[log_middleware])
-    app.router.add_post("/webhook", handle_webhook)
-    app.on_startup.append(on_startup)
-    
-    port = int(os.getenv("PORT", 8000))
-    web.run_app(app, host="0.0.0.0", port=port)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
